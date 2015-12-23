73a442c43b371017ae2fe03686ea5c30768310c8
Andrew Ahern <AndyYankee17@gmail.com>
2015-12-23 15:15:48 +0000

    Shiny Tutorial

--- ui.R
+++ ui.R
@@ -11,7 +11,7 @@ shinyUI(pageWithSidebar(
     sliderInput("obs", 
                 "Number of observations:", 
                 min = 1,
-                max = 2500, 
+                max = 1000, 
                 value = 500)
   ),
 
