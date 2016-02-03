c8772524062810de9a98e581de34ce519ff11a14
Andrew Ahern <AndyYankee17@gmail.com>
2016-02-03 22:15:01 +0000

    Added some missing files

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Working !</h1>
+      </div>
+    )
+  }
+}
