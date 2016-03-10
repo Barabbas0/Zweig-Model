0d4cdf94e09ec6a830589a17dcce4dd4bad6dc18
Andrew Ahern <AndyYankee17@gmail.com>
2016-03-10 15:09:51 +0000

    Resolved left over reference

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
